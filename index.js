6c7fdd126cf95ebc71d3cd34757764a7b4a258ae
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2024-12-10 00:39:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 4000;
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+// routes
+
+app.get('/', (req, res) => {
+    res.send('server is running!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+
+
