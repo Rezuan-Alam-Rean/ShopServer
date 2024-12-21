724f736823e821e1212301a20bfdfaf9ce6bc339
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2024-12-22 02:12:03 +0600

    setup server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 4000;
@@ -10,7 +11,30 @@ app.use(cors());
 app.use(express.json());
 
 
+//database connection
 
+uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ndxdp2s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 
 // routes
@@ -20,7 +44,7 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(` app listening on port 4000 ${port}`)
+    console.log(` app listening on port  ${port}`)
 })
 
 
